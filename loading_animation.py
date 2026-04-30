5e443a894f713682d39a47ec4d4800000e92d1d0
Venomsec45 <dayshaunmarnin@gmail.com>
2026-04-30 14:50:18 +0800

    Added to improve introduction

--- loading_animation.py
+++ loading_animation.py
@@ -2,37 +2,7 @@ import sys
 import os
 import time
 import json
-
-# Dayshaun part, ignore mo nalang ito
-def check_codes():
-	main_directory = [
-		root
-		for root, _, _ in os.walk(os.path.dirname(__file__))
-	]
-
-	subdirectories = [
-		directory
-		for _, directory, _ in os.walk(os.path.dirname(__file__))
-	]
-
-	files = [
-		file
-		for _, _, files in os.walk(os.path.dirname(__file__))
-		for file in files
-	]
-
-	if not os.path.exists(f"{os.path.dirname(__file__)}/.project_codes_snapshot.json"):
-		snapshot = {
-			"Main directory": main_directory,
-			"Subdirectories": subdirectories,
-			"Files": files
-		}
-
-		with open(f"{os.path.dirname(__file__)}/.project_codes_snapshot.json", "w") as file:
-			json.dump(snapshot, file, indent=4)
-
-
-check_codes()
+from color import Colors
 
 # Edson part
 def loading_animation():
@@ -45,8 +15,9 @@ def loading_animation():
 
 		bar = '█' * filled_length + '-' * (bar_length - filled_length)
 
-		print(f"\rloading: |{bar}| {i}%", end="")
+		print(f"\r{Colors.Custom_blue}LOADING: {Colors.End}|{bar}| {i}%", end="")
 		sys.stdout.flush()
-		time.sleep(0.03)
+		time.sleep(0.1)
+		
 	
 	print("\nSkynet accessed successfully!\n")
