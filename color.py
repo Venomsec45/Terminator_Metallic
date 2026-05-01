8d72fd60fbb1a792703b7b1640faa64fe7723917
Venomsec45 <dayshaunmarnin@gmail.com>
2026-05-01 12:03:51 +0800

    Added a custom yellow color

--- color.py
+++ color.py
@@ -1,10 +1,19 @@
 class Colors:
-	Red = "\033[31m"
-	Green = "\033[32m"
-	Grey = "\033[38;2;107;107;107m"
-	Yellow = "\033[33m"
 	Blue = "\033[34m"
 	Custom_blue = "\033[38;2;32;42;232m"
+
+	Green = "\033[32m"
+	Custom_green_1 = "\033[38;2;55;232;32m"
+
+	Yellow = "\033[33m"
+	Custom_yellow = "\033[38;2;239;242;15m"
+
+	Red = "\033[31m"
+
+	Grey = "\033[38;2;107;107;107m"
+	
 	Bold = "\033[1m"
+	
 	Underline = "\033[4m"
+	
 	End = "\033[0m"
