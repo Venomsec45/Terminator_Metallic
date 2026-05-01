6c16c4bcea6bf6a04ab9ba92fbc71a69f4b3a2a3
Venomsec45 <dayshaunmarnin@gmail.com>
2026-05-01 15:56:08 +0800

    Few changes such as adding loop to loop functions and try except is added to handle errors

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-import os
-import subprocess as sub
 import time
 import sys
-from developer_logo import logo
-from loading_animation import loading_animation
+from additional_scripts.screen_clear import terminal_screen_clear
+from color import Colors
+from developer_logo import *
+from loading_animation import *
 
-sub.run("clear" if os.name == "posix" else "cls")
-print(logo())
-time.sleep(2)
-loading_animation()
+# Logo
+try:
+    for logo_function_call in [logo, logo_2]:
+        terminal_screen_clear()
+        logo_function_call()
+        time.sleep(3)
+
+    # Looping functions for cleanliness
+    terminal_screen_clear()
+    for function_call in [power_animation, status_animation, skynet_activate]:
+        function_call()
+
+except KeyboardInterrupt:
+    print(f"{Colors.Custom_red}Game stopped{Colors.End}")
+    sys.exit(2)
