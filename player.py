fea3e6495a831de5a2249600374844db7f7fe32f
Venomsec45 <dayshaunmarnin@gmail.com>
2026-05-10 21:53:51 +0800

    Removed a line of code for storing username

--- player.py
+++ player.py
@@ -21,13 +21,17 @@ class Character(ABC):
 
 # Player class
 class Player(Character):
-    def __init__(self, hp=100, damage=15):
+
+    def __init__(self, username, hp=100, damage=15):
         super().__init__(hp, damage)
+
+        self.username = username
         self.max_hp = hp
         self.xp = 0
         self.coins = 50
         self.inventory = []
         self.max_inventory = 4
+
 
     # Combat system
     def attack(self, target):
@@ -35,34 +39,46 @@ class Player(Character):
         print(f"\nYou attack for {dmg} damage!")
         target.take_damage(dmg)
 
+
     def take_damage(self, amount):
         self.hp -= amount
+
         if self.hp < 0:
             self.hp = 0
+
         print(f"You took {amount} damage! HP: {self.hp}/{self.max_hp}")
+
 
     def heal(self, amount):
         self.hp += amount
+
         if self.hp > self.max_hp:
             self.hp = self.max_hp
+
         print(f"Healed {amount} HP! HP: {self.hp}/{self.max_hp}")
+
 
     # Items
     def add_item(self, item):
+
         if len(self.inventory) >= self.max_inventory:
             print("Inventory is full!")
             return False
 
         self.inventory.append(item)
+
         print(f"{item} added to inventory.")
         return True
 
+
     def use_item(self):
+
         if not self.inventory:
             print("No items available!")
             return
 
         print("\nInventory:")
+
         for i, item in enumerate(self.inventory, 1):
             print(f"{i}. {item}")
 
@@ -90,20 +106,25 @@ class Player(Character):
         else:
             print("Unknown item.")
 
+
     # Progression system
     def gain_rewards(self, xp, coins):
         self.xp += xp
         self.coins += coins
+
         print(f"\nGained {xp} XP and {coins} coins!")
         print(f"XP: {self.xp} | Coins: {self.coins}")
+
 
     # Utility
     def reset_hp(self):
         self.hp = self.max_hp
         print("HP fully restored!")
 
+
     def show_status(self):
         print("\n--- PLAYER STATUS ---")
+        print(f"Username: {self.username}")
         print(f"HP: {self.hp}/{self.max_hp}")
         print(f"XP: {self.xp}")
         print(f"Coins: {self.coins}")
