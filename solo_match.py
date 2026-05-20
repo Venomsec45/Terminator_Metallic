7dcc61b78cac7de4536bfd8671d0f1712babceb1
Venomsec45 <dayshaunmarnin@gmail.com>
2026-05-20 09:46:47 +0800

    Added few colors and a terminal screen clearer

--- solo_match.py
+++ solo_match.py
@@ -1,7 +1,8 @@
 from player import Player
 from enemy import random_enemy
 from color import Colors
-
+import time
+from additional_scripts.screen_clear import terminal_screen_clear
 
 # Solo Match Function
 def solo_match(player):
@@ -12,10 +13,12 @@ def solo_match(player):
 
     # Combat Loop
     while player.is_alive() and enemy.is_alive():
+        terminal_screen_clear()
 
-        print("\n" + "-" * 40 + "STATUS" + "-" * 40)
+        print("\n" + "-" * 40 + "STATUS" + "-" * 40 + "\n")
         print(f"Your HP: {Colors.Custom_green_1}{player.hp}{Colors.End}/{Colors.Custom_green_1}{player.max_hp}{Colors.End}")
-        print(f"{enemy.name} HP: {enemy.hp}")
+        print(f"{enemy.name} HP: {Colors.Custom_green_1}{enemy.hp}{Colors.End}")
+        print("\n" + "-" * 86)
 
         print(f"\n1. {Colors.Custom_red}Attack{Colors.End}")
         print(f"2. {Colors.Custom_blue}Use Item{Colors.End}")
@@ -38,9 +41,10 @@ def solo_match(player):
 
         # Enemy turn
         enemy.attack(player)
+        time.sleep(2.5)
 
     # Results
-    print("\n" + "-" * 40 + "RESULT" + "-" * 40)
+    print("\n" + "-" * 40 + "RESULT" + "-" * 40 + "\n")
 
     if player.hp <= 0:
         print(f"{Colors.Custom_red}You lost the match!{Colors.End}")
@@ -49,15 +53,16 @@ def solo_match(player):
         print(f"{Colors.Custom_green_1}You won the match!{Colors.End}")
         player.gain_rewards(enemy.xp_reward, enemy.coin_reward)
 
+    print("\n" + "-" * 86)
+    time.sleep(2.5)
+
     # Reset HP
     player.reset_hp()
 
-    print("HP restored.\n")
+    print(f"{Colors.Custom_green_1}HP RESTORED{Colors.End}\n")
 
 
 # Test
 if __name__ == "__main__":
-
     player = Player("Player1")
-
     solo_match(player)
